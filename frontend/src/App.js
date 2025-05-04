3cd16491318cb2569969f98b7a0e54b16a49951b
Saathvika <ratnasaathvikavaranasir18@gmail.com>
2025-05-04 12:39:25 +0530

    Git website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Header from './components/Header';
 import AuthButtons from './components/AuthButtons';
